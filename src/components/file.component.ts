267a8d8fd24c02209a5db7cac4e024064a4fdc08
Fachri Hawari <fachri.hawari@gmail.com>
2023-09-30 07:22:28 +0700

    Revert "feat: add mongodb type support (#49)"
    This reverts commit e26b4e14351cbd0e1cfbfb1a8fb6254e83125847.

--- src/components/file.component.ts
+++ src/components/file.component.ts
@@ -101,12 +101,6 @@ export class FileComponent implements Echoable {
 			})
 		})
 
-		if (this.prismaClass.types) {
-			this.prismaClass.types.forEach(type => {
-				this.registerImport(type + "Type", "./" + type.toLowerCase() + "_type")
-			})
-		}
-
 		if (generator.getConfig().useGraphQL) {
 			this.registerImport('ID', '@nestjs/graphql')
 			this.registerImport('Int', '@nestjs/graphql')
